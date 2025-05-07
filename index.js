d111247f2e99aab319a1500816042d72e86b08b7
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-05-08 04:53:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,9 @@ const graphqlEndpoint = 'https://graphql.union.build/v1/graphql';
 const baseExplorerUrl = 'https://sepolia.etherscan.io';
 const unionUrl = 'https://app.union.build/explorer';
 
-const rpcProviders = [new JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/n8IrzxdL7dh_zcA_8MoTYcqrCLPMQAUv')];
+const rpcProviders = [
+  new ethers.providers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/n8IrzxdL7dh_zcA_8MoTYcqrCLPMQAUv')
+];
 let currentRpcProviderIndex = 0;
 
 function provider() {
