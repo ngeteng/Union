e8fde7f92866dcc04ebd01bf298ef3934b422136
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-05-08 04:54:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { sendReport } = require('./telegramReporter');
-const { ethers, JsonRpcProvider } = require('ethers');
+const { ethers } = require('ethers');
 const axios = require('axios');
 const moment = require('moment-timezone');
 const readline = require('readline');
